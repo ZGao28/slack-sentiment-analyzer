f06dc0702e778e6f3cb72b8921e4b67fb4e681cc
Zi Gao <zi.s.gao@gmail.com>
2019-04-25 23:51:00 -0400

    get channel list

---
+++ backend/Conversations.js
@@ -0,0 +1,8 @@
+const getConversations = async (channelList) => {
+  console.log(channelList);
+  return channelList;
+}
+
+module.exports = {
+  getConversations,
+}
--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,32 @@
 const express = require('express');
-const dotenv = require('dotenv');
+const dotenv = require('dotenv').config();
 const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
+const { getConversations } = require('./Conversations');
+const { getSentiments } = require('./AnalyzeSentiment');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get('/data', async (req, res) => {
+  const data = await fetch(`https://slack.com/api/conversations.list`, {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + process.env.SLACK_TOKEN,
+    }
+  });
+  if (data.status === 200) {
+    const channelData = await data.json();
+    const conversations = await getConversations(channelData.channels);
+    const sentiments = await getSentiments(conversations);
+    res.status(200).json(sentiments);
+  } else {
+    res.status(500).send('Something broke!')
+  }
+});
 
 
 app.listen(8081, () => {
