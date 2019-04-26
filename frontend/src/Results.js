a11c3f6010d9fee91e7d219e5ac54cab0ea7ba57
Dervis Cetinalp <derviscetinalp@gmail.com>
2019-04-26 13:27:28 -0400

    Display messages

--- frontend/src/Results.js
+++ frontend/src/Results.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import Paper from '@material-ui/core/Paper';
+// import Paper from '@material-ui/core/Paper';
 
 import Menu from './Menu';
 import Graph from './Graph';
+import Table from './Table';
 import './Results.css'
 
 class Results extends Component {
@@ -42,19 +43,14 @@ class Results extends Component {
   render() {
     const { tones, sentenceTones } = this.props;
     return (
-      <div style={{ display: 'flex', flexDirection: 'column', width: '100%', maxWidth: '1400px' }}>
+      <div style={{ display: 'flex', flexDirection: 'column', width: '100%', maxWidth: '1400px', marginBottom: '100px' }}>
         <div className='container'>
           <Menu changeCategory={this.changeCategory}></Menu>
           <Graph tones={tones} sentenceTones={sentenceTones} showDetails={this.showDetails}></Graph>
         </div>
-        <div>
-          <Paper>
-            <h2>Sup duck</h2>
-            <h2>Sup duck</h2>
-            <h2>Sup duck</h2>
-            <h2>Sup duck</h2>
-          </Paper>
-        </div>
+        {this.state.toneInfo.length > 0 &&
+          <Table toneInfo={this.state.toneInfo}></Table>
+        }
       </div>
     )
   }
