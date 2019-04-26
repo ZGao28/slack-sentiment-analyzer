8f9ec4b1a9c8d1f9912044be4eb59192439fd64c
Dervis Cetinalp <derviscetinalp@gmail.com>
2019-04-25 23:37:08 -0400

    add results view

---
+++ frontend/src/Results.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Results extends Component {
+  render() {
+    return (
+      <h1>Insert Results Here</h1>
+    )
+  }
+}
+
+export default Results;
